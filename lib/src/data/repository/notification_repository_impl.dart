4ade501d54ee4eb37ecc9443dc29a20fbf09e7cc
Andres Ruiz <amestr367@gmail.com>
2022-02-20 11:18:12 -0500

    logica de notificaciones

--- lib/src/data/repository/notification_repository_impl.dart
+++ lib/src/data/repository/notification_repository_impl.dart
@@ -1,22 +1,27 @@
-
 import 'package:domo_server/src/data/datasource/data_source_data.dart';
 import 'package:domo_server/src/domain/repository/repository_domain.dart';
 
-
-class NotificationRepositoryImpl implements NotificationRepositoryDomain{
-final NotificationRemoteDataSource firebaseNotificationRemote;
+class NotificationRepositoryImpl implements NotificationRepositoryDomain {
+  final NotificationRemoteDataSource firebaseNotificationRemote;
 
   NotificationRepositoryImpl(this.firebaseNotificationRemote);
- @override
-  Stream<dynamic> backGround()=> firebaseNotificationRemote.backGround();
+  @override
+  Stream<dynamic> backGround() => firebaseNotificationRemote.backGround();
 
   @override
   Stream<dynamic> foreGround() => firebaseNotificationRemote.foreGround();
 
   @override
-  Stream<dynamic> terminated()=> firebaseNotificationRemote.terminated();
+  Stream<dynamic> terminated() => firebaseNotificationRemote.terminated();
 
   @override
-  Future<bool> sendNotification({required Map<String, dynamic> body, required String token, required String title})=>firebaseNotificationRemote.sendNotification(body: body, token: token, title: title);
-
+  Future<bool> sendNotification(
+          {required Map<String, dynamic> body,
+          required String token,
+          required String title}) =>
+      firebaseNotificationRemote.sendNotification(
+        body: body,
+        token: token,
+        title: title,
+      );
 }
